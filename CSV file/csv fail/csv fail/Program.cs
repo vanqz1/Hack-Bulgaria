c56232242ad9ada4b0efe48cb55b8096df1afa97
vanq <vanqz1@outlook.com>
2015-02-20 00:18:47 +0200

    CSV file

---
+++ CSV file/csv fail/csv fail/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\test.csv";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string[] mask;
+                string s = " ";
+
+
+                int numcol = 0;
+                int check = 0;
+                int numRow = 0;
+                //// Save data from CSV file in a list
+                List<List<string>> data = new List<List<string>>();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    numRow++;
+                    check++;
+                    int countCreate = 0;
+                    int i = 0;
+                    mask = s.Split(' ', ',');
+                    if (check == 1)
+                    {
+                        numcol = mask.Length;
+                    }
+                    if (countCreate == 0)
+                    {
+
+                        for (int j = 0; j < mask.Length; j++)
+                        {
+                            data.Add(new List<string>());
+                        }
+                        countCreate++;
+                    }
+
+                    foreach (var element in mask)
+                    {
+                        data[i].Add(element);
+                        i++;
+                    }
+
+                    i = 0;
+
+
+                }
+                /// List of names of cols
+                List<string> listcols = new List<string>();
+                for (int i = 0; i < numcol; i++)
+                {
+
+                    listcols.Add(data[i][0]);
+                }
+
+
+                string query = Console.ReadLine();
+                string[] commands = Regex.Split(query, @"\s+");
+
+
+
+                /// the switch takes the first command of user
+                switch (commands[0])
+                {
+                    case "SHOW":
+                        {
+                            for (int i = 0; i < numcol; i++)
+                            {
+                                Console.Write("|");
+                                Console.Write(data[i][0]);
+                                Console.Write("|");
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
+                    case "FIND":
+                        {
+                            string contain = commands[1];
+                            List<int> foundRow = new List<int>();
+                            for (int i = 0; i < numRow; i++)
+                            {
+                                for (int k = 0; k < numcol; k++)
+                                {
+                                    if (data[k][i].Contains(contain))
+                                    {
+                                        foundRow.Add(i);
+
+                                        for (int m = 0; m < numcol; m++)
+                                        {
+                                            Console.Write(data[m][i]);
+                                            Console.Write(" ");
+                                        }
+                                        Console.WriteLine(" ");
+                                        
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case "SUM":
+                        {
+                            int choosencol = 0;
+
+                            for (int i = 0; i < listcols.Count; i++)
+                            {
+                                if (listcols[i] == commands[1])
+                                {
+                                    choosencol = i;
+                                }
+
+                            }
+                            int sum = 0;
+                            for (int i = 0; i < data[choosencol].Count; i++)
+                            {
+                                int x;
+                                if (int.TryParse(data[choosencol][i], out x))
+                                {
+                                    sum += int.Parse(data[choosencol][i]);
+                                }
+                            }
+                            Console.WriteLine(sum);
+                            break;
+                        }
+                    case "SELECT":
+                        {
+
+                            List<int> result = new List<int>();
+                            int choosencol = 0;
+                            ////Check whether the query contains LIMIT
+                            int lim = 0;
+                            for (int i = 1; i < commands.Length; i++)
+                            {
+                                for (int k = 0; k < commands.Length; k++)
+                                {
+                                    if (commands[k] == "LIMIT")
+                                    {
+                                        lim = int.Parse(commands[k + 1]);
+                                    }
+                                }
+
+                            }
+
+                            for (int i = 1; i < commands.Length; i++)
+                            {
+                                for (int k = 0; k < listcols.Count; k++)
+                                {
+                                    /// Checks whick cols are chosen
+                                    if (commands[i] == listcols[k])
+                                    {
+                                        choosencol = k;
+                                        result.Add(k);
+                                    }
+                                }
+                            }
+
+                            ////Prints the table the if/else statements determines whether there is a limit or no 
+                            if (lim == 0)
+                            {
+                                for (int m = 0; m < numRow; m++)
+                                {
+                                    for (int i = 0; i < result.Count; i++)
+                                    {
+                                        Console.Write("|");
+                                        Console.Write(data[result[i]][m]);
+                                        Console.Write("|");
+                                    }
+                                    Console.WriteLine(" ");
+
+                                }
+                            }
+                            else
+                            {
+                                for (int m = 0; m < lim; m++)
+                                {
+                                    for (int i = 0; i < result.Count; i++)
+                                    {
+                                        Console.Write("|");
+                                        Console.Write(data[result[i]][m]);
+                                        Console.Write("|");
+                                    }
+                                    Console.WriteLine(" ");
+
+                                }
+
+                            }
+
+
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Please write correct query!");
+                            break;
+
+                        }
+                }
+
+            }
+        }
+    }
+}
